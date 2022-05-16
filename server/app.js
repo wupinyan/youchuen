0970949b03f454d6982af13bddf785b5b2700d8f
pinyen <yan830803@gmail.com>
2022-05-16 10:11:24 +0800

    .

--- server/app.js
+++ server/app.js
@@ -4,7 +4,14 @@ const path = require('path')
 
 app.use('/api', require('./init-router') )
 
+if (process.env.NODE_ENV === 'production') {
+    app.use( express.static('dist') )
+    app.get('*', function (req, res) {
+        res.sendFile(path.resolve(__dirname,'../dist/index.html'))
+    })
+}
+
 const port = process.env.PORT || 8081
 app.listen(port, function () {
-    console.log(`The server is running with ${process.env.NODE_ENV} mode`); 
+    console.log(`port ${port}. ${process.env.NODE_ENV} mode`); 
 })
