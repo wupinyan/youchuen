cdb6a2352507f0597f995ba32ff23262f0d7de1e
pinyen <yan830803@gmail.com>
2022-05-19 15:23:05 +0800

    modify fullend to frontend

---
+++ src/store/commodity-list.js
@@ -0,0 +1,45 @@
+export default {
+    namespaced: true,
+
+    state:{
+        commodityList: {
+            1:{
+                name: '電動代步車'
+            },
+            2:{
+                name: '病床'
+            },
+            3:{
+                name: '輪椅'
+            },
+            4:{
+                name: '血壓計'
+            },
+            5:{
+                name: '血糖機'
+            },
+            6:{
+                name: '血氧機'
+            },
+            7:{
+                name: '便椅'
+            },
+            8:{
+                name: '拐杖'
+            },
+            9:{
+                name: '氧氣機'
+            },
+            10:{
+                name: '抽痰機'
+            },
+            11:{
+                name: '化痰機'
+            },
+            12:{
+                name: '額耳溫槍'
+            },
+        }
+    }
+
+}
