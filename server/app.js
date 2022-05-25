cffac550457fab4c10da10b1d8fe09a850726c60
pinyen <yan830803@gmail.com>
2022-05-25 14:05:57 +0800

    .

--- server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/api', require('./init-router') )
-
-const port = process.env.PORT || 8081
-app.listen(port, function () {
-    console.log(`The server is running with ${process.env.NODE_ENV} mode`); 
-})
