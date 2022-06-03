3d34087a1f1d8d5665aab3e0478f12a45bc70d52
pinyen <yan830803@gmail.com>
2022-06-03 11:16:43 +0800

    .

---
+++ src/pages/Connection/Meta.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="meta">
+        <h3>
+            <slot name="name"/>
+        </h3>
+        <p>
+            <slot name="scription"/>
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../../assets/style.scss';
+.meta {
+    border-radius: 4px;
+    margin: 5vw auto;
+    padding: 12px 40px;
+    width: fit-content;
+    background-color: rgba($color1, .7);
+    text-align: center;
+    h3 {
+        border-bottom: solid orange;
+        margin: 0;
+    }
+    p {
+        margin: 0;
+        font-weight: 900;
+    }
+}
+</style>
