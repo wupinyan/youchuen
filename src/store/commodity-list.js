a719763320878ddcd837096e1c97bc7a5a3196c8
pinyen <yan830803@gmail.com>
2022-06-07 10:23:30 +0800

    .

--- src/store/commodity-list.js
+++ src/store/commodity-list.js
@@ -4,40 +4,50 @@ export default {
     state:{
         commodityList: {
             1:{
-                name: '電動代步車'
+                name: '輪椅',
+                commodity:[
+                    { name:'康揚 5001', img:'wheelchair1.png' },
+                    { name:'康揚 養樂多515', img:'wheelchair2.png' },
+                    { name:'康揚 飛揚825', img:'wheelchair3.png' },
+                    { name:'均佳 150', img:'wheelchair4.webp' },
+                    { name:'杏華 JR200S', img:'wheelchair5.png' },
+                ]
             },
             2:{
-                name: '病床'
+                name: '便盆椅'
             },
             3:{
-                name: '輪椅'
+                name: '單支拐杖'
             },
             4:{
-                name: '血壓計'
+                name: '四腳拐'
             },
             5:{
-                name: '血糖機'
+                name: '助行器'
             },
             6:{
-                name: '血氧機'
+                name: '沐浴椅'
             },
             7:{
-                name: '便椅'
+                name: '氣墊床'
             },
             8:{
-                name: '拐杖'
+                name: '輪椅坐墊'
             },
             9:{
-                name: '氧氣機'
+                name: '電動床'
             },
             10:{
-                name: '抽痰機'
+                name: '移位用品'
             },
             11:{
-                name: '化痰機'
+                name: '電動車'
             },
             12:{
-                name: '額耳溫槍'
+                name: '電動輪椅'
+            },
+            13:{
+                name: '氧氣機'
             },
         }
     }
