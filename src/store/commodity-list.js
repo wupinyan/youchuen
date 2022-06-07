91a01ce6d3cf755ccef7ba0c9116f77e05255daf
pinyen <yan830803@gmail.com>
2022-06-07 15:41:15 +0800

    .

--- src/store/commodity-list.js
+++ src/store/commodity-list.js
@@ -6,24 +6,50 @@ export default {
             1:{
                 name: '輪椅',
                 commodity:[
-                    { name:'康揚 5001', img:'wheelchair1.png' },
-                    { name:'康揚 養樂多515', img:'wheelchair2.png' },
-                    { name:'康揚 飛揚825', img:'wheelchair3.png' },
-                    { name:'均佳 150', img:'wheelchair4.webp' },
-                    { name:'杏華 JR200S', img:'wheelchair5.png' },
+                    { name:'康揚 5001', img:'wheelchair1.png', scription:'椅背可平躺，腿部可抬起，扶手可掀起，輪椅可拆卸、收合。' },
+                    { name:'康揚 仰樂多515', img:'wheelchair2.png', scription:'椅背可平躺，扶手可掀起，輪椅可拆卸、收合。' },
+                    { name:'康揚 飛揚825', img:'wheelchair3.png', scription:'扶手可掀起，輪椅可拆卸、收合。' },
+                    { name:'均佳 150', img:'wheelchair4.webp', scription:'輪椅可收合。' },
+                    { name:'杏華 JR200S', img:'wheelchair5.png', scription:'輪椅可收合。' },
                 ]
             },
             2:{
-                name: '便盆椅'
+                name: '便盆椅',
+                commodity:[
+                    { name:'富士康 4301', img:'commodechair1.jpg', scription:'有輪子，可推移。' },
+                    { name:'富士康 4542', img:'commodechair2.jpg', scription:'有輪子，可推移，可收合。' },
+                    { name:'富士康 4351', img:'commodechair3.jpg', scription:'有輪子，可推移，椅背可仰躺至45度角。' },
+                    { name:'富士康 4527', img:'commodechair4.jpg', scription:'可收合。' },
+                    { name:'均佳 103', img:'commodechair5.webp', scription:'可收合。' },
+                    { name:'杏華 BM1', img:'commodechair6.jpg', scription:'' }, 
+                ]
             },
             3:{
-                name: '單支拐杖'
+                name: '單支拐杖',
+                commodity:[
+                    { name:'杏華 7501', img:'single-crutch1.png', scription:'可調整拐杖長短。' },
+                    { name:'杏華 7533', img:'single-crutch2.png', scription:'可調整拐杖長短，可摺疊收合。' },
+                    { name:'富士康 2204', img:'single-crutch3.jpg', scription:'可調整拐杖長短，有四爪腳墊，可站立。' },
+                    { name:'富士康 2205', img:'single-crutch4.jpg', scription:'可調整拐杖長短，有四爪腳墊，可站立。' },
+                ]
             },
             4:{
-                name: '四腳拐'
+                name: '四腳拐',
+                commodity:[
+                    { name:'富士康 2053', img:'fourlegged-crutch1.jpg', scription:'可調整拐杖長短。' },
+                    { name:'富士康 2058', img:'fourlegged-crutch2.jpg', scription:'可調整拐杖長短。' },
+                    { name:'富士康 2036', img:'fourlegged-crutch3.jpg', scription:'可調整拐杖長短。' },
+                    { name:'富士康 2037', img:'fourlegged-crutch4.jpg', scription:'可調整拐杖長短。' },
+                    { name:'富士康 2051', img:'fourlegged-crutch5.jpg', scription:'可調整拐杖長短。' },
+                ]
             },
             5:{
-                name: '助行器'
+                name: '助行器',
+                commodity:[
+                    { name:'富士康 3428', img:'walker1.jpg', scription:'可收合。' },
+                    { name:'富士康 3133', img:'walker2.jpg', scription:'可收合。' },
+                    { name:'富士康 833', img:'walker3.jpg', scription:'可收合，有輪子，手把處可調高低，手把處有剎車。' },
+                ]
             },
             6:{
                 name: '沐浴椅'
