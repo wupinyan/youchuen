f4005658cc90ebfd049eaae2f1c28e2792e20f4b
pinyen <yan830803@gmail.com>
2022-06-12 10:57:24 +0800

    .

---
+++ src/pages/Commodity/Item.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="item">
+        <img :src="commodity.img" alt="暫無圖片">
+        <div class="text">
+            <h1>{{commodity.name}}</h1>
+            <p>
+                型號: <br>
+                {{commodity.model}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['commodity'],
+    mounted() {
+        //console.log(this.commodity)
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/style.scss';
+.item {
+    border-radius: 4px;
+    margin: 16px auto;
+    padding: 5%;
+    width: 270px;
+    background: white;
+    text-align: center;
+    img{
+        $size: 180px;
+        width: $size;
+        height: $size;
+        max-width: $size;
+        max-height: $size;
+    }
+    .text {
+        h1 {
+            border-bottom: solid orange;
+            margin: 0;
+            color: $color1;
+        }
+        p {
+            margin: 0;
+            font-weight: 900;
+        }
+    }
+}
+</style>
--- src/pages/Commodity/index.vue
+++ src/pages/Commodity/index.vue
@@ -1,5 +1,42 @@
 <template>
     <main>
-        commodity
+        <h2>{{infoList.name}}</h2>
+        <ul>
+            <Item v-for="(com, key) in commodityList" :commodity='com' :key='key'/>
+        </ul>
+        <!-- <Item :commodity='commodityList[0]'/>
+        <Item :commodity='commodityList[1]'/>
+        <Item :commodity='commodityList[2]'/> -->
     </main>
 </template>
+
+<script>
+import Item from './Item'
+export default {
+    computed:{
+        infoList(){
+            const { $route, $store } = this
+            const id = $route.params.id
+            return $store.state.commodityList
+                .commodityList[id] 
+        },
+        commodityList(){
+            return this.infoList.commodity.map( v =>{
+                const img = require('../../assets/commodity-images/' + v.img)
+                v.img = img
+                return v
+            })
+        },
+    },
+    components: { Item },
+    mounted() {
+        //console.log(this.commodityList);
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+    background: #eeeeee;
+}
+</style>
