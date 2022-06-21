6e78035bad5b3e561a42dfa015e07b009ea67abc
pinyen <yan830803@gmail.com>
2022-06-21 10:24:03 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/youchuen/'
-    : '/'
+    : '/',
 })
